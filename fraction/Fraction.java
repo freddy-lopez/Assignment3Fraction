771bc57f50c8884f48aff7eb6947aa4c06d7aefd
freddy-lopez <74562226+freddy-lopez@users.noreply.github.com>
2020-12-01 16:02:51 -0800

    Initial commit

---
+++ fraction/Fraction.java
@@ -0,0 +1,144 @@
+package fraction;
+//What's left to do is the Argument Exceptions and compareTo()and realValue() methods. 
+public class Fraction {
+
+	private int Num;
+	private int Den;
+	private double Value;
+	
+	public Fraction(int N, int D)
+	{
+		if(D == 0)
+		{
+			throw new IllegalArgumentException();
+		}
+		Num = N;
+		Den = D;
+		//properReduction call
+		reduction();
+		
+	}
+	private void reduction() {
+		if(Num == 0)
+		{
+			Den = 1;
+			return;
+		}
+		if(Den < 0)
+		{
+			Num = -Num;
+			Den = -Den;
+		}
+		int q = GCF(Math.abs(Num), Den);
+		Num /= q;
+		Den /= q;
+		
+	}
+	//GCF method used for the reduction method
+	public int GCF(int num, int den)
+	{
+		if(num % den == 0)
+		{
+			return den;
+		}
+		else if(den % num == 0)
+		{
+			return num;
+		}
+		else {
+			return GCF(num % den, den % num);
+		}
+	}
+	//Done
+	/**
+	* @return the Numerater that is Num
+	*/
+	public int getNum()
+	{
+		return Num;
+	}
+	//Done
+	/**
+	* @return the Denominator that is Den
+	*/
+	public int getDen()
+	{
+		return Den;
+	}
+	//Done
+	@Override
+	public boolean equals(Object obj)
+	{
+		Fraction frac = (Fraction)obj;
+		if(obj instanceof Fraction)
+		{
+			if(this.Num == frac.Num && this.Den == frac.Den)
+			{
+				return true;
+			}
+			else {
+				return false;
+			}
+
+		}
+		else {
+			return false;
+		}
+	}
+	//
+	public int compareTo(Object o) 
+	{
+		//Method TODO
+		return 0;
+	}
+	
+	public double realValue() 
+	{
+		//Method TODO
+		return 0;
+	}
+	
+	
+	
+	
+	//Done
+	//GCD method to be used in fraction GCD
+	public int GCD(int a, int b)
+	{ 
+		return b==0 ? a : GCD(b, a%b); 
+	}
+
+	//Done
+	public Fraction fractionGCD(int n1, int n2){
+		int g = GCD(n1,n2);
+		n1 = n1/g;
+		n2 = n2/g;
+		return new Fraction(n1,n2);
+	}
+
+	//Done
+	public Fraction add(Fraction fraction) {
+		int firstAdd = (this.Num*fraction.Den)+(this.Den*fraction.Num);
+		int secondAdd = this.Den*fraction.Den;
+		return fractionGCD(firstAdd, secondAdd);
+	}
+	//Done
+	public Fraction multiply(Fraction fraction) {
+		int firstMultiply = this.Num*fraction.Num;
+		int secondMultiply = this.Den*fraction.Den;
+		return fractionGCD(firstMultiply, secondMultiply);
+	}
+	
+
+	@Override 
+	public String toString(){
+		String One = Integer.toString(this.Num); //convert fraction to string
+		String Two = Integer.toString(this.Den);
+		if (Two == "1"){ //check for whole number
+			return One;
+		}else{
+			String Tog = One + "/" + Two; 
+			return Tog;
+		}
+	}
+}
